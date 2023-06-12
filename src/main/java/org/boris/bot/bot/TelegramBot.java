92929ee86413aa1a189797b036a125953cc8cdda
boris <boris919323@gmail.com>
2023-06-12 13:59:09 +0300

    init commit

---
+++ src/main/java/org/boris/bot/bot/TelegramBot.java
@@ -0,0 +1,65 @@
+package org.boris.bot.bot;
+
+import lombok.AllArgsConstructor;
+import org.boris.bot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig botConfig;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String text = update.getMessage().getText();
+        long chatId = update.getMessage().getChatId();
+
+        System.out.println(chatId);
+        switch (text) {
+            case "/start":
+                sendMessage(chatId, "Привет человек");
+                break;
+            case "/love":
+                sendMessage(chatId, "Я счастлив, что бог дал мне глаза, и я смог разглядеть тебя в толпе. И благодарен ему за возможность называть тебя своей половинкой.");
+        }
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+        super.onUpdatesReceived(updates);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onRegister() {
+        super.onRegister();
+    }
+
+    private void sendMessage(Long chatId, String textToSend) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/boris/bot/config/BotConfig.java
@@ -0,0 +1,20 @@
+package org.boris.bot.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource("classpath:application.properties")
+public class BotConfig {
+
+    @Value("${bot.name}")
+    private String botName;
+
+    @Value("${bot.token}")
+    private String token;
+}
+
+
