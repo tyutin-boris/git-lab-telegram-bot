dc86ee3481482535c22f30bef3d7117ec537f160
Тютин Борис Сергеевич <btyutin@phoenixit.ru>
2023-06-22 15:29:37 +0300

    wip

--- src/main/java/org/boris/bot/controller/MergeRequestController.java
+++ src/main/java/org/boris/bot/controller/MergeRequestController.java
@@ -1,7 +1,8 @@
 package org.boris.bot.controller;
 
-import org.boris.bot.api.MergeRequest;
-import org.springframework.http.MediaType;
+import lombok.SneakyThrows;
+import org.boris.bot.bot.TelegramBot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/merges/requests")
 public class MergeRequestController {
 
+    @Autowired
+    private final TelegramBot bot;
+
+    public MergeRequestController(TelegramBot bot) {this.bot = bot;}
+
+    @SneakyThrows
     @PostMapping
     public void mergeRequestEvent(@RequestBody String request) {
 //    public void mergeRequestEvent(@RequestBody String request) {
-        System.out.println(request);
+//        System.out.println(request);
+
+        bot.sendAction();
     }
 }
