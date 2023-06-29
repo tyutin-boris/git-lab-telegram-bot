bafd254ea3aacf1ff9ab3a47e4483304170ae312
Тютин Борис Сергеевич <btyutin@phoenixit.ru>
2023-06-29 17:02:48 +0300

    fix

--- src/main/java/org/boris/bot/controller/MergeRequestController.java
+++ src/main/java/org/boris/bot/controller/MergeRequestController.java
@@ -1,14 +1,14 @@
 package org.boris.bot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.boris.bot.api.MergeRequest;
 import org.boris.bot.services.MergeRequestService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/merges/requests")
 public class MergeRequestController {
@@ -21,6 +21,7 @@ public class MergeRequestController {
 
     @PostMapping
     public void mergeRequestEvent(@RequestBody MergeRequest request) {
+        log.info(request.toString());
         mergeRequestService.sendMergeRequest(request);
     }
 }
--- src/main/java/org/boris/bot/services/MergeRequestServiceImpl.java
+++ src/main/java/org/boris/bot/services/MergeRequestServiceImpl.java
@@ -1,22 +1,24 @@
 package org.boris.bot.services;
 
 import org.boris.bot.api.MergeRequest;
+import org.boris.bot.api.ObjectAttributes;
+import org.boris.bot.api.Project;
 import org.boris.bot.api.Reviewer;
 import org.boris.bot.senders.MergeRequestSender;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.util.CollectionUtils;
 
 import java.time.OffsetDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class MergeRequestServiceImpl implements MergeRequestService {
 
+    private static final String DEFAULT_PREFIX = "Тут могло быть ";
     private final MergeRequestSender sender;
-
     private final String chatId = "586815794";
-
 
     public MergeRequestServiceImpl(MergeRequestSender sender) {
         this.sender = sender;
@@ -24,28 +26,40 @@ public class MergeRequestServiceImpl implements MergeRequestService {
 
     @Override
     public void sendMergeRequest(MergeRequest request) {
-        String projectName = request.getProject().getName();
-        String title = request.getObjectAttributes().getTitle();
-        String description = request.getObjectAttributes().getDescription();
-        OffsetDateTime createdAt = request.getObjectAttributes().getCreatedAt();
-        String sourceBranch = request.getObjectAttributes().getSourceBranch();
-        String targetBranch = request.getObjectAttributes().getTargetBranch();
+        String projectName = Optional.ofNullable(request.getProject())
+                .map(Project::getName)
+                .orElse(DEFAULT_PREFIX + "название проекта.");
+        String title = Optional.ofNullable(request.getObjectAttributes())
+                .map(ObjectAttributes::getTitle)
+                .orElse(DEFAULT_PREFIX + "название МРа.");
+        String description = Optional.ofNullable(request.getObjectAttributes())
+                .map(ObjectAttributes::getDescription)
+                .orElse(DEFAULT_PREFIX + "описание МРа");
+        OffsetDateTime createdAt = Optional.ofNullable(request.getObjectAttributes())
+                .map(ObjectAttributes::getCreatedAt)
+                .orElse(OffsetDateTime.now());
+        String sourceBranch = request.getObjectAttributes()
+                .getSourceBranch();
+        String targetBranch = request.getObjectAttributes()
+                .getTargetBranch();
 
-        String username = request.getUser().getUsername();
-        String name = request.getUser().getName();
+        String username = request.getUser()
+                .getUsername();
+        String name = request.getUser()
+                .getName();
 
-        List<Reviewer> reviewers = request.getReviewers();
-        Reviewer reviewer = reviewers.stream().findFirst().orElse(null);
-        String reviewerName = Optional.ofNullable(reviewer).map(Reviewer::getName).orElse("");
+        List<Reviewer> reviewers = Optional.ofNullable(request.getReviewers()).orElse(Collections.emptyList());
+        Reviewer reviewer = reviewers.stream()
+                .findFirst()
+                .orElse(null);
+        String reviewerName = Optional.ofNullable(reviewer)
+                .map(Reviewer::getName)
+                .orElse("");
 
-        String text = "Project: " + projectName + "\n\n" +
-                "Title: " + title + "\n\n" +
-                "Description: " + description + "\n\n" +
-                "Create date: " + createdAt + "\n\n" +
-                "Autor: " + name + "\n\n" +
-                "Username: " + username + "\n\n" +
-                "Reviewer: " + reviewerName + "\n\n" +
-                "Source branch: " + sourceBranch + " ==> " + "Target branch: " + targetBranch;
+        String text = "Project: " + projectName + "\n\n" + "Title: " + title + "\n\n" + "Description: " + description +
+                "\n\n" + "Create date: " + createdAt + "\n\n" + "Autor: " + name + "\n\n" + "Username: " + username +
+                "\n\n" + "Reviewer: " + reviewerName + "\n\n" + "Source branch: " + sourceBranch + " ==> " +
+                "Target branch: " + targetBranch;
 
         sender.sendMessage(text, chatId);
     }
