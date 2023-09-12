65ae1daccdf001b50e4228565844e46d86574745
Тютин Борис Сергеевич <btyutin@phoenixit.ru>
2023-09-12 18:38:27 +0300

    add detailedMergeStatus

--- src/main/java/ru/git/lab/bot/api/mr/DetailedMergeStatus.java
+++ src/main/java/ru/git/lab/bot/api/mr/DetailedMergeStatus.java
@@ -1,4 +1,17 @@
 package ru.git.lab.bot.api.mr;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum DetailedMergeStatus {
+
+    MERGEABLE("mergeable"),
+    NOT_OPEN("not_open"),
+    DRAFT_STATUS("draft_status"),
+    CHECKING("checking"),
+    CI_STILL_RUNNING("ci_still_running");
+
+    private final String name;
 }
