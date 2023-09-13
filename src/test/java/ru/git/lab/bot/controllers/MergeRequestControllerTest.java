3909230c3d9d6381a85c867b63781548a4a24980
Тютин Борис Сергеевич <btyutin@phoenixit.ru>
2023-09-13 13:41:26 +0300

    add test

---
+++ src/test/java/ru/git/lab/bot/controllers/MergeRequestControllerTest.java
@@ -0,0 +1,14 @@
+package ru.git.lab.bot.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public class MergeRequestControllerTest {
+
+    @Test
+    public void mergeRequestEvent() {
+    }
+}
