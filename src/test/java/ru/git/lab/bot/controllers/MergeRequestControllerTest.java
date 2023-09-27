da2cde02fd2b6f8b12da58aa24ab71b8a203daec
Boris <52064056+tyutin-boris@users.noreply.github.com>
2023-09-27 11:38:58 +0300

    Add test for approved and unapproved (#2)
    * wip
    
    * add test for approve
    
    * add test for unapproved
    
    ---------
    
    Co-authored-by: Тютин Борис Сергеевич <btyutin@phoenixit.ru>

--- src/test/java/ru/git/lab/bot/controllers/MergeRequestControllerTest.java
+++ src/test/java/ru/git/lab/bot/controllers/MergeRequestControllerTest.java
@@ -1,18 +1,23 @@
 package ru.git.lab.bot.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import ru.git.lab.bot.dto.ChatType;
+import ru.git.lab.bot.model.entities.ApproveEntity;
 import ru.git.lab.bot.model.entities.ChatEntity;
 import ru.git.lab.bot.model.entities.GitUserEntity;
 import ru.git.lab.bot.model.entities.MessageEntity;
+import ru.git.lab.bot.model.repository.ApproveRepository;
 import ru.git.lab.bot.model.repository.ChatRepository;
 import ru.git.lab.bot.model.repository.MessageRepository;
 import ru.git.lab.bot.model.repository.UserRepository;
+import ru.git.lab.bot.services.mr.handlers.MrApprovedEventHandler;
 import ru.git.lab.bot.services.mr.handlers.MrOpenEventHandler;
+import ru.git.lab.bot.services.mr.handlers.MrUnapprovedEventHandler;
 import ru.git.lab.bot.services.senders.api.MessageSender;
 
 import java.io.IOException;
@@ -21,11 +26,13 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.OffsetDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest
@@ -34,8 +41,12 @@ public class MergeRequestControllerTest {
 
     private final long chatId = -1001833741964L;
 
+    private final long mrId = 235411851L;
+
+    private final long authorId = 14826841L;
+
     @Autowired
-    private MergeRequestController mergeRequestController;
+    private MergeRequestController sut;
 
     @Autowired
     private ChatRepository chatRepository;
@@ -46,20 +57,33 @@ public class MergeRequestControllerTest {
     @Autowired
     private MessageRepository messageRepository;
 
+    @Autowired
+    private ApproveRepository approveRepository;
+
     @SpyBean
     private MrOpenEventHandler mrOpenEventHandler;
 
     @SpyBean
+    private MrApprovedEventHandler mrApprovedEventHandler;
+
+    @SpyBean
+    private MrUnapprovedEventHandler mrUnapprovedEventHandler;
+
+    @SpyBean
     private MessageSender messageSender;
+
+    @BeforeEach
+    public void setUp() {
+        createChat();
+    }
 
     @Test
     public void shouldSendMessageToTgWhenCreteNewMR() {
         //given
         String message = getMessage("mr/open.json");
-        createChat();
 
         //when
-        mergeRequestController.mergeRequestEvent(message);
+        sut.mergeRequestEvent(message);
 
         //then
         verify(mrOpenEventHandler).handleEvent(any());
@@ -73,10 +97,9 @@ public class MergeRequestControllerTest {
     public void shouldNotSendMessageToTgWhenCreteNewMRWithDraftStatus() {
         //given
         String message = getMessage("mr/open_draft.json");
-        createChat();
 
         //when
-        mergeRequestController.mergeRequestEvent(message);
+        sut.mergeRequestEvent(message);
 
         //then
         verify(mrOpenEventHandler).handleEvent(any());
@@ -92,9 +115,60 @@ public class MergeRequestControllerTest {
         assertThat(messageEntity).isNull();
     }
 
+    @Test
+    public void shouldSendApproveToMessage() {
+        //given
+        String openMessage = getMessage("mr/open.json");
+        sut.mergeRequestEvent(openMessage);
+
+        String approvedMessage = getMessage("mr/approved.json");
+
+        //when
+        sut.mergeRequestEvent(approvedMessage);
+
+        //then
+        verify(mrApprovedEventHandler).handleEvent(any());
+        verify(messageSender).updateMessage(any(), eq(chatId), any());
+
+        checkApproveSave();
+    }
+
+    @Test
+    public void shouldSendUnapprovedToMessage() {
+        //given
+        String openMessage = getMessage("mr/open.json");
+        sut.mergeRequestEvent(openMessage);
+
+        String approvedMessage = getMessage("mr/approved.json");
+        sut.mergeRequestEvent(approvedMessage);
+
+        String unapprovedMessage = getMessage("mr/unapproved.json");
+
+        //when
+        sut.mergeRequestEvent(unapprovedMessage);
+
+        //then
+        verify(mrUnapprovedEventHandler).handleEvent(any());
+        verify(messageSender, times(2)).updateMessage(any(), eq(chatId), any());
+
+        checkApproveDelete();
+    }
+
+    private void checkApproveDelete() {
+        List<ApproveEntity> approves = approveRepository.findAllByMrIdAndAuthorId(mrId, authorId);
+        assertThat(approves).isEmpty();
+    }
+
+    private void checkApproveSave() {
+        List<ApproveEntity> approves = approveRepository.findAllByMrIdAndAuthorId(mrId, authorId);
+        ApproveEntity actualApprove = approves.stream()
+                .findFirst()
+                .orElse(null);
+        assertThat(actualApprove).isNotNull();
+        assertThat(actualApprove.getAuthorName()).isEqualTo("Ivan Ivanov");
+    }
+
     private void checkMessageSave() {
-        Long mrId = 235411851L;
-        Long authorId = 14826841L;
         MessageEntity messageEntity = messageRepository.findByMrIdAndAuthorId(mrId, authorId)
                 .orElse(null);
 
