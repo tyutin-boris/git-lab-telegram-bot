5b4546d5446d857eacb6952b9622fabe3fb53123
Тютин Борис Сергеевич <btyutin@phoenixit.ru>
2023-10-20 14:57:06 +0300

    Add new message chats table

--- src/main/java/ru/git/lab/bot/model/entities/TgMrMessageEntity.java
+++ src/main/java/ru/git/lab/bot/model/entities/TgMrMessageEntity.java
@@ -5,12 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.OffsetDateTime;
+import java.util.Set;
 
 @Entity
 @Setter
@@ -24,27 +27,25 @@ public class TgMrMessageEntity {
     @Column(nullable = false)
     private Long id;
 
-    @Column(name = "tg_id")
-    private Integer tgId;
-
-    @Column(name = "chat_id")
-    private Long chatId;
-
     @Column(name = "mr_id", nullable = false)
     private Long mrId;
 
     @Column(name = "author_id", nullable = false)
     private Long authorId;
 
-    @Column(name ="text")
+    @Column(name = "text")
     private String text;
 
-    @Column(name ="is_draft")
+    @Column(name = "is_draft")
     private boolean isDraft;
 
-    @Column(name ="is_delete")
+    @Column(name = "is_delete")
     private boolean isDelete;
 
     @Column(name = "create_date_time", nullable = false)
     private OffsetDateTime createDateTime;
+
+    @OneToMany
+    @JoinColumn(name = "tg_mr_messages_id")
+    private Set<MessageChatsEntity> chats;
 }
