1eaa9ac34a02de6e7990a8e9335c488be34d2878
Тютин Борис Сергеевич <btyutin@phoenixit.ru>
2023-10-24 15:45:15 +0300

    Add pipeline status for private message

--- src/main/java/ru/git/lab/bot/services/pipelines/PipelineServiceImpl.java
+++ src/main/java/ru/git/lab/bot/services/pipelines/PipelineServiceImpl.java
@@ -3,11 +3,16 @@ package ru.git.lab.bot.services.pipelines;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.git.lab.bot.api.mr.User;
+import ru.git.lab.bot.api.pipeline.PipeObjectAttributes;
 import ru.git.lab.bot.api.pipeline.PipelineEvent;
 import ru.git.lab.bot.api.pipeline.PipelineStatus;
 import ru.git.lab.bot.model.repository.TgGitUsersRepository;
 import ru.git.lab.bot.services.pipelines.api.PipelineService;
+import ru.git.lab.bot.services.pipelines.api.PipelineTestMessageService;
 import ru.git.lab.bot.services.senders.api.MessageSender;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -16,12 +21,12 @@ public class PipelineServiceImpl implements PipelineService {
 
     private final MessageSender messageSender;
 
+    private final PipelineTestMessageService pipelineTestMessageService;
+
     private final TgGitUsersRepository tgGitUsersRepository;
 
     @Override
     public void handle(PipelineEvent event) {
-        System.out.println();
-
         PipelineStatus status = event.getObjectAttributes()
                 .getStatus();
 
@@ -35,18 +40,36 @@ public class PipelineServiceImpl implements PipelineService {
     }
 
     private void sendPipelineStatusNotification(PipelineEvent event) {
+        Optional<PipelineEvent> eventOpt = Optional.ofNullable(event);
 
-        PipelineStatus status = event.getObjectAttributes()
-                .getStatus();
-        String username = event.getUser()
-                .getUsername();
+        Optional<PipelineStatus> status = eventOpt.map(PipelineEvent::getObjectAttributes)
+                .map(PipeObjectAttributes::getStatus);
 
-        tgGitUsersRepository.findByGitUsername(username)
-                .forEach(user -> {
-                    String text = "Piple status " + status;
-                    Long chatId = -1002018570984L;
-                    messageSender.sendMessage(text, chatId);
-                });
+        if (status.isEmpty()) {
+            log.error("Pipeline notification no sent. Pipeline status not found.");
+            return;
+        }
+
+        Optional<String> username = eventOpt.map(PipelineEvent::getUser)
+                .map(User::getUsername);
+
+        if (username.isEmpty()) {
+            log.error("Pipeline notification no sent. Username not found.");
+            return;
+        }
+
+        boolean isDraft = event.getMergeRequest()
+                .getTitle()
+                .contains("Draft: ");
+
+        if (!isDraft) {
+            tgGitUsersRepository.findByGitUsername(username.get())
+                    .forEach(user -> {
+                        String text = pipelineTestMessageService.createText(event);
+                        Long chatId = user.getTgId();
+                        messageSender.sendMessage(text, chatId);
+                    });
+        }
     }
 
     private void pipelineLog(Long mrId, PipelineStatus status) {
